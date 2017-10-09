e491d5e17cbe7c1430aad6ea275d748371b7f923
Mayuresh Shilotri <mayuresh.shilotri@gmail.com>
2017-10-09 17:41:38 +0530

    Update test_q04_spearman_correlation.py

--- q04_spearman_correlation/tests/test_q04_spearman_correlation.py
+++ q04_spearman_correlation/tests/test_q04_spearman_correlation.py
@@ -5,14 +5,14 @@ from inspect import getargspec
 
 class TestLoad_regression_plot(TestCase):
     def test_correlation(self):
-        spear_corr = spearman_correlation()
-        args = getargspec(spearman_correlation)
 
         # Input parameters tests
+        args = getargspec(spearman_correlation)
         self.assertEqual(len(args[0]), 0, "Expected arguments %d, Given %d" % (0, len(args[0])))
         self.assertEqual(args[3], None, "Expected default values do not match given default values")
 
         # Return type tests
+        spear_corr = spearman_correlation()
         self.assertIsInstance(spear_corr, float, "Expected data type for `Spearman correlation` is float you are returning\
          %s" % (type(spear_corr)))
 
