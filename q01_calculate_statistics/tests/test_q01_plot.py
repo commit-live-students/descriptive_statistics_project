5c7acc1c64761943ff8b3bb9596b8339ea7c4e40
Mayuresh Shilotri <mayuresh.shilotri@gmail.com>
2017-10-09 17:38:14 +0530

    Update test_q01_plot.py

--- q01_calculate_statistics/tests/test_q01_plot.py
+++ q01_calculate_statistics/tests/test_q01_plot.py
@@ -6,14 +6,13 @@ from ..build import calculate_statistics
 
 class TestLoad_distplot(TestCase):
     def test_calculate_statistics(self):
-        mean, median, mode = calculate_statistics()
-        args = getargspec(calculate_statistics)
-
         # Input parameters tests
+        args = getargspec(calculate_statistics)
         self.assertEqual(len(args[0]), 0, "Expected arguments %d, Given %d" % (0, len(args[0])))
         self.assertEqual(args[3], None, "Expected default values do not match given default values")
 
         # Return type tests
+        mean, median, mode = calculate_statistics()
         self.assertIsInstance(mean, float, "Expected data type for mean is float you are returning %s" % (type(mean)))
         self.assertIsInstance(median, float, "Expected data type for median is float you are returning %s" % (type(median)))
         self.assertIsInstance(mode, numpy.int64, "Expected data type for mode is numpy.int64 you are returning %s" % (type(mode)))
