f6b92e57faacfedb3b2e6d9d604483a9de91c915
Mayuresh Shilotri <mayuresh.shilotri@gmail.com>
2017-10-09 17:50:19 +0530

    Update build.py

--- q04_spearman_correlation/build.py
+++ q04_spearman_correlation/build.py
@@ -2,13 +2,7 @@
 import pandas as pd
 
 dataframe_1 = pd.read_csv('data/house_prices_multivariate.csv')
-house_price = dataframe_1.loc[:, 'SalePrice']
-
 dataframe_2 = pd.read_csv('data/house_prices_copy.csv')
-weight_of_nasa_space_shuttle = dataframe_2.loc[:, 'SalePrice']
 
-
-def spearman_correlation():
-    # Enter code here
-
+# Your code here
 
