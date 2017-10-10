2c54c47a1cf64776ca9f80e29c4c7f9ec02ec0ee
msonumanish <msonu.manish@gmail.com>
2017-10-10 18:14:06 +0000

    Done

--- q01_calculate_statistics/build.py
+++ q01_calculate_statistics/build.py
@@ -1,3 +1,4 @@
+# %load q01_calculate_statistics/build.py
 # Default Imports
 import numpy as np
 import pandas as pd
@@ -7,4 +8,15 @@ sale_price = data.loc[:, "SalePrice"]
 
 # Return mean,median & mode for the SalePrice Column
 # Write your code here
+def calculate_statistics():
+    sale_price_mean=sale_price.mean()
+    sale_price_median=np.median(sale_price).item()
+    #.astype('float')
+    sale_price_mode=sale_price.mode()
+    return(sale_price_mean,sale_price_median,sale_price_mode[0])
 
+
+
+#(mean, float, "Expected data type for mean is float you are returning %s" % (type(mean)))
+#(median, float, "Expected data type for median is float you are returning %s" % (type(median)))
+#(mode, numpy.int64, "Expected data type for mode is numpy.int64 you are returning %s" % (type(mode)))
