0c79404d9a04529fc5440059c4341b8169f1a1ba
satishkhanna <snykhanna@gmail.com>
2017-10-10 17:08:59 +0000

    Done

--- q01_calculate_statistics/build.py
+++ q01_calculate_statistics/build.py
@@ -5,6 +5,11 @@ import pandas as pd
 data = pd.read_csv('data/house_prices_multivariate.csv')
 sale_price = data.loc[:, "SalePrice"]
 
+def calculate_statistics():
+    mean = sale_price.mean()
+    median = sale_price.median()
+    mode = sale_price.mode()
+    print(mode)
+    return mean ,median ,mode
 # Return mean,median & mode for the SalePrice Column
 # Write your code here
-
--- q02_plot/build.py
+++ q02_plot/build.py
@@ -6,5 +6,19 @@ from greyatomlib.descriptive_stats.q01_calculate_statistics.build import calcula
 dataframe = pd.read_csv('data/house_prices_multivariate.csv')
 sale_price = dataframe.loc[:, 'SalePrice']
 
-
+def plot():
+    mean = sale_price.mean()
+    median = sale_price.median()
+    mode_temp = sale_price.mode()
+    mode = mode_temp.item()
+    plt.hist(sale_price)
+    plt.axvline(mean)
+    plt.show()
+    plt.hist(sale_price)
+    plt.axvline(median)
+    plt.show()
+    plt.hist(sale_price)
+    plt.axvline(mode)
+    plt.show()
+    
 # Draw the plot for the mean, median and mode for the dataset
