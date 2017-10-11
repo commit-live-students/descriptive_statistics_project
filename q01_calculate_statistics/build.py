23f6cc95618d7994d158ddf6261bf15d735003bf
mritunjaysingh07 <mj.singh07@gmail.com>
2017-10-11 17:34:42 +0000

    Done

--- q01_calculate_statistics/build.py
+++ q01_calculate_statistics/build.py
@@ -2,9 +2,15 @@
 import numpy as np
 import pandas as pd
 
-data = pd.read_csv('data/house_prices_multivariate.csv')
-sale_price = data.loc[:, "SalePrice"]
+def calculate_statistics() :
+
+    data = pd.read_csv('data/house_prices_multivariate.csv')
+    sale_price = data.loc[:, "SalePrice"]
+    print (sale_price.mean())
+    print (sale_price.median())
+    print (sale_price.mode())
+
+
 
 # Return mean,median & mode for the SalePrice Column
 # Write your code here
-
--- q02_plot/build.py
+++ q02_plot/build.py
@@ -1,10 +1,20 @@
 # Default Imports
 import pandas as pd
 import matplotlib.pyplot as plt
-from greyatomlib.descriptive_stats.q01_calculate_statistics.build import calculate_statistics
+#from greyatomlib.descriptive_stats.q01_calculate_statistics.build import calculate_statistics
+def plot() :
+    dataframe = pd.read_csv('data/house_prices_multivariate.csv')
+    sale_price = dataframe.loc[:, 'SalePrice']
+    #print(sale_price)
+    dataframe.hist(column = 'SalePrice')
+    mean = sale_price.mean()
+    median = sale_price.median()
+    mode = sale_price.mode()
+    plt.axvline(mean, color = 'g', linestyle = 'dashed', linewidth = 2)
+    plt.axvline(median, color = 'r', linestyle = 'dashed', linewidth = 2)
+    plt.axvline(mean, color = 'b', linestyle = 'dashed', linewidth = 2)
+    plt.show()
 
-dataframe = pd.read_csv('data/house_prices_multivariate.csv')
-sale_price = dataframe.loc[:, 'SalePrice']
 
 
 # Draw the plot for the mean, median and mode for the dataset
