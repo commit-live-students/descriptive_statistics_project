3f575ce1427125639102cd010c061bee68c85622
gaurav-barclays <gaurav.simply@gmail.com>
2017-10-13 04:15:01 +0000

    Done

--- q01_calculate_statistics/build.py
+++ q01_calculate_statistics/build.py
@@ -1,10 +1,15 @@
+# %load q01_calculate_statistics/build.py
 # Default Imports
 import numpy as np
 import pandas as pd
 
 data = pd.read_csv('data/house_prices_multivariate.csv')
 sale_price = data.loc[:, "SalePrice"]
-
+def calculate_statistics():
+    x=data["SalePrice"].median()
+    y=data["SalePrice"].mean()
+    z=data["SalePrice"].mode()[0]
+    return (y,x,z)
+#calculate_statistics()
 # Return mean,median & mode for the SalePrice Column
 # Write your code here
-
