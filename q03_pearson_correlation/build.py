a901207a389d7ec0f3a1144faf86b8be85ff61f2
Sagar-saga <sagar.parthe@gmail.com>
2017-10-18 17:05:13 +0000

    Done

--- q03_pearson_correlation/build.py
+++ q03_pearson_correlation/build.py
@@ -1,5 +1,6 @@
 # Default Imports
 import pandas as pd
+import numpy as np
 
 dataframe_1 = pd.read_csv('data/house_prices_multivariate.csv')
 dataframe_2 = pd.read_csv('data/house_prices_copy.csv')
