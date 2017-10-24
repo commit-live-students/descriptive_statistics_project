cac1312b88431716c178cc523802dfa89672c8cf
Rahul0211 <rahuljain86@hotmail.com>
2017-10-24 12:38:17 +0000

    Done

--- q02_plot/build.py
+++ q02_plot/build.py
@@ -1,11 +1,13 @@
 # Default Imports
 import pandas as pd
+#matplotlib.use('agg')
 import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.pyplot.switch_backend('agg')
 from greyatomlib.descriptive_stats.q01_calculate_statistics.build import calculate_statistics
 
 dataframe = pd.read_csv('data/house_prices_multivariate.csv')
 sale_price = dataframe.loc[:, 'SalePrice']
-
 
 def plot():
     mean = sale_price.mean()
