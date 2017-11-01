7c7e3545fccbebf9d495b217772f58f559d3467a
vinod-boga <vinodboga17@gmail.com>
2017-11-01 16:51:33 +0000

    Done

--- q02_plot/build.py
+++ q02_plot/build.py
@@ -1,13 +1,21 @@
 # Default Imports
 import pandas as pd
 import numpy as np
+import matplotlib
+matplotlib.use('agg')
 import matplotlib.pyplot as plt
 from greyatomlib.descriptive_stats.q01_calculate_statistics.build import calculate_statistics
 
-dataframe = pd.read_csv('data/house_prices_multivariate.csv')
-sale_price = dataframe.loc[:, 'SalePrice']
+data = pd.read_csv('data/house_prices_multivariate.csv')
+sale_price = data.loc[:, "SalePrice"]
 
-plt.axvline(x=sale_price.mean(), color='k', linestyle='--')
-plt.axvline(x=sale_price.median(), color='k', linestyle='--')
-plt.axvline(x=sale_price.mode(), color='k', linestyle='--')
-plt.show()
+def plot():
+    mean = sale_price.mean()
+    median = sale_price.median()
+    mode = sale_price.mode()
+    plt.hist(sale_price)
+    plt.axvline(mean, color='k', linestyle='--')
+    plt.axvline(median, color='r', linestyle='--')
+    plt.axvline(mode[0], color='b', linestyle='--')
+    plt.show()
+plot()
