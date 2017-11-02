05bb3c2a403d50237a8ec2b0b4140d1b9afc6f28
utk1801 <g.utk1801@gmail.com>
2017-11-02 19:01:44 +0000

    Done

--- q03_pearson_correlation/build.py
+++ q03_pearson_correlation/build.py
@@ -1,8 +1,8 @@
 # Default Imports
 import pandas as pd
 
-dataframe_1 = pd.read_csv('data/house_prices_multivariate.csv')
-dataframe_2 = pd.read_csv('data/house_prices_copy.csv')
+dataframe_1 = pd.read_csv('/home/utk1801/Workspace/code/descriptive_statistics_project/data/house_prices_multivariate.csv')
+dataframe_2 = pd.read_csv('/home/utk1801/Workspace/code/descriptive_statistics_project/data/house_prices_copy.csv')
 
 
 # Return the correlation value between the SalePrice column for the two loaded datasets
@@ -10,3 +10,4 @@ dataframe_2 = pd.read_csv('data/house_prices_copy.csv')
 def correlation():
     corr1 = dataframe_1['SalePrice'].corr(dataframe_2['SalePrice'])
     return corr1
+correlation()
