79a3bd38e2e9be89d339f82f933c5d0d4b52b7eb
darshind <doshi.darshin@gmail.com>
2017-11-11 17:14:56 +0000

    Done

--- q02_plot/build.py
+++ q02_plot/build.py
@@ -1,3 +1,4 @@
+# %load q02_plot/build.py
 # Default Imports
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -8,3 +9,13 @@ sale_price = dataframe.loc[:, 'SalePrice']
 
 
 # Draw the plot for the mean, median and mode for the dataset
+def plot():
+        mean = sale_price.mean()
+        median = sale_price.median()
+        mode= sale_price.mode()
+        sale_price.plot(kind='hist')
+        plt.axvline(mean,color='b', linestyle='dashed', linewidth=2)
+        plt.axvline(median,color='g', linestyle='dashed', linewidth=2)
+        plt.axvline(mode[0],color='b', linestyle='dashed', linewidth=2)
+        plt.show()
+        return
--- q03_pearson_correlation/build.py
+++ q03_pearson_correlation/build.py
@@ -1,9 +1,13 @@
 # Default Imports
 import pandas as pd
 
-dataframe_1 = pd.read_csv('data/house_prices_multivariate.csv')
-dataframe_2 = pd.read_csv('data/house_prices_copy.csv')
+dataframe_1 = pd.read_csv('/home/darshind/Workspace/code/descriptive_statistics_project/data/house_prices_multivariate.csv')
+dataframe_2 = pd.read_csv('/home/darshind/Workspace/code/descriptive_statistics_project/data/house_prices_copy.csv')
 
 
 # Return the correlation value between the SalePrice column for the two loaded datasets
 # Your code here
+def correlation():
+    return dataframe_1.corr()['SalePrice'],dataframe_2.corr()['SalePrice']
+
+print correlation()
