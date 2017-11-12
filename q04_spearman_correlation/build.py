6ecae52391c0ab8a5b44aa8105bfcccb84ca58a5
darshind <doshi.darshin@gmail.com>
2017-11-12 04:34:57 +0000

    Done

--- q04_spearman_correlation/build.py
+++ q04_spearman_correlation/build.py
@@ -1,8 +1,16 @@
-# Default Import
+# %load q03_pearson_correlation/build.py
+# Default Imports
 import pandas as pd
+from scipy.stats import spearmanr
 
 dataframe_1 = pd.read_csv('data/house_prices_multivariate.csv')
+house_price = dataframe_1.loc[:,'SalePrice']
 dataframe_2 = pd.read_csv('data/house_prices_copy.csv')
+house_price2 = dataframe_2.loc[:,'SalePrice']
 
+# Return the correlation value between the SalePrice column for the two loaded datasets
 # Your code here
-
+def spearman_correlation():
+    a = spearmanr(house_price,house_price2)
+    b = a.correlation
+    return b
