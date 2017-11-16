2f7ea15cdb5dba47b9768ee870671d755c129df2
ankutty <akutty@deloitte.com>
2017-11-16 01:37:10 +0000

    Done

--- q04_spearman_correlation/build.py
+++ q04_spearman_correlation/build.py
@@ -1,8 +1,12 @@
 # Default Import
 import pandas as pd
+import scipy.stats
 
 dataframe_1 = pd.read_csv('data/house_prices_multivariate.csv')
+rank_1 = dataframe_1['SalePrice'].rank()
 dataframe_2 = pd.read_csv('data/house_prices_copy.csv')
+rank_2 = dataframe_2['SalePrice'].rank()
 
 # Your code here
-
+def spearman_correlation():
+    return scipy.stats.spearmanr(rank_1,rank_2)[0]
