d99e96692a1ee73e24dc71ed9e48d45ebb652ccc
Bijit Deka <bijitdeka23@gmail.com>
2017-12-13 17:11:53 +0530

    modified q1 test

--- q01_calculate_statistics/tests/test_q01_plot.py
+++ q01_calculate_statistics/tests/test_q01_plot.py
@@ -13,17 +13,29 @@ class TestLoad_distplot(TestCase):
         args = getargspec(calculate_statistics)
         self.assertEqual(args[3], None, "Expected default values do not match given default values")
 
-    def test_calculate_statistics_result_type(self):  # Return type tests
+    def test_calculate_statistics_mean_type(self):  # Return type tests
         mean, median, mode = calculate_statistics()
         self.assertIsInstance(mean, float, "Expected data type for mean is float you are returning %s" % (type(mean)))
+
+    def test_calculate_statistics_median_type(self):
+        mean, median, mode = calculate_statistics()
         self.assertIsInstance(median, float,
                               "Expected data type for median is float you are returning %s" % (type(median)))
+
+    def test_calculate_statistics_mode_type(self):  # Return type tests
+        mean, median, mode = calculate_statistics()
         self.assertIsInstance(mode, numpy.int64,
                               "Expected data type for mode is numpy.int64 you are returning %s" % (type(mode)))
 
 
-    def test_calculate_statistics_result_values(self):  # Return value tests
+    def test_calculate_statistics_mean_values(self):  # Return value tests
         mean, median, mode = calculate_statistics()
         self.assertAlmostEqual(mean, 185479.51124002901, 2, "Return `mean` value does not match expected value")
+
+    def test_calculate_statistics_median_values(self):  # Return value tests
+        mean, median, mode = calculate_statistics()
         self.assertAlmostEqual(median, 167500.0, 1, "Return `median` value does not match expected value")
+
+    def test_calculate_statistics_mode_values(self):  # Return value tests
+        mean, median, mode = calculate_statistics()
         self.assertAlmostEqual(mode, 140000, "Return `mode` value does not match expected value")
