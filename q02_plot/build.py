978d09119716979a782286014b147bdf69bc535e
NeelkanthMehta <neilstrong2003@yahoo.com>
2017-12-16 18:11:10 +0000

    Done

--- q02_plot/build.py
+++ q02_plot/build.py
@@ -8,4 +8,10 @@ sale_price = dataframe.loc[:, 'SalePrice']
 
 
 # Draw the plot for the mean, median and mode for the dataset
-def plot()
+def plot():
+    plt.hist(Sales_price, bins=20, color='c')
+    plt.axvline(x=Sales_price.mean(), color='b', linestyle='dashed', linewidth=2)
+    plt.axvline(x=Sales_price.median(), color='red', linestyle='dashed', linewidth=2)
+    plt.axvline(x=Sales_price.mode()[0], color='r', linestyle='dashed', linewidth=2)
+    return plt.show()
+    
