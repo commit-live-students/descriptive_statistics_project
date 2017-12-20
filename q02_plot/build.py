37bd4a18d6adbab8f3c3d270583f23867106f3f7
prateek15Student <prateek+15@greyatom.com>
2017-12-20 14:01:06 +0000

    Done

--- q02_plot/build.py
+++ q02_plot/build.py
@@ -1,4 +1,3 @@
-# Default Imports
 import pandas as pd
 import matplotlib.pyplot as plt
 from greyatomlib.descriptive_stats.q01_calculate_statistics.build import calculate_statistics
@@ -6,6 +5,30 @@ from greyatomlib.descriptive_stats.q01_calculate_statistics.build import calcula
 dataframe = pd.read_csv('data/house_prices_multivariate.csv')
 sale_price = dataframe.loc[:, 'SalePrice']
 
+def plot():
+    #Get the mean, median and mode from calculate_statistics function for saleprice for housing Iowa
+    args=calculate_statistics()
 
-# Draw the plot for the mean, median and mode for the dataset
+    #figure(figsize=(1,1)) creates an inch-by-inch image, which will be 80-by-80 pixels unless you also give a different dpi argument.
+    plt.figure(figsize=(10, 6))
+
+    #plot the histogram for sale_price {bins = range of values on x axis and y denotes the count of elements falling between those range}
+    plt.hist(sale_price, bins=40)
+
+    #plot the mean by value on x being mode/mean/median plotted 300 times while on y being plotted 0-299 times to get a straight line
+    plt.plot([args[0]]*300, range(300), label='mode')
+    plt.plot([args[1]]*300, range(300), label='median')
+    plt.plot([args[2]]*300, range(300), label='mean')
+
+    #limit the length of y till 250 only
+    plt.ylim(0, 250)
+
+    #plot the lables as well defined in mean, median and mode plotting
+    plt.legend()
+
+    #finally show the histogram along with mean, median and mode
+    plt.show
+plot()
+
+
 
