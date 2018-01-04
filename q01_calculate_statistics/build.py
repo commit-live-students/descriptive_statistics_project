b39c9503c691d56feaea99e52d3cb47f6112ad18
maswath89 <maswath89@gmail.com>
2018-01-04 17:31:35 +0000

    Done

--- q01_calculate_statistics/build.py
+++ q01_calculate_statistics/build.py
@@ -6,6 +6,10 @@ data = pd.read_csv('data/house_prices_multivariate.csv')
 sale_price = data.loc[:, "SalePrice"]
 
 
-# Return mean,median & mode for the SalePrice Column
-# Write your code here
+def calculate_statistics():
+    mean = np.mean(sale_price)
+    median = np.median(sale_price)
+    mode = np.array([sale_price.mode()])
+    return mean, median, mode.astype('int64')
 
+calculate_statistics()
--- q03_pearson_correlation/build.py
+++ q03_pearson_correlation/build.py
@@ -1,9 +1,12 @@
 # Default Imports
 import pandas as pd
+import numpy as np
 
 dataframe_1 = pd.read_csv('data/house_prices_multivariate.csv')
 dataframe_2 = pd.read_csv('data/house_prices_copy.csv')
 
+def correlation():
+    r = np.corrcoef(dataframe_1.SalePrice, dataframe_2.SalePrice)[0, 1]
+    return r
 
-# Return the correlation value between the SalePrice column for the two loaded datasets
-# Your code here
+correlation()
