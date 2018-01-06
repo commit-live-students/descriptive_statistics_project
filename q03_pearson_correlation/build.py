27376a25873134aaac46af01fd90e8a46df264e2
Hazi1990 <hazi1990@gmail.com>
2018-01-06 18:45:47 +0000

    Done

--- q03_pearson_correlation/build.py
+++ q03_pearson_correlation/build.py
@@ -1,9 +1,12 @@
 # Default Imports
 import pandas as pd
-
+import numpy as np
 dataframe_1 = pd.read_csv('data/house_prices_multivariate.csv')
 dataframe_2 = pd.read_csv('data/house_prices_copy.csv')
 
-
+def correlation():
+    sale_price1 = dataframe_1.loc[:, "SalePrice"]
+    sale_price2 = dataframe_2.loc[:, "SalePrice"]
+    return np.corrcoef(sale_price1,sale_price2)[0,1]
 # Return the correlation value between the SalePrice column for the two loaded datasets
 # Your code here
