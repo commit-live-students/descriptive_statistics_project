9ae02fc1b286662721a2bb6fe3159fbeda0453b0
brijesh-upadhyay <brijesh_upadhyay@ymail.com>
2018-01-27 07:27:09 +0000

    Done

--- q03_pearson_correlation/build.py
+++ q03_pearson_correlation/build.py
@@ -1,5 +1,8 @@
+# %load q03_pearson_correlation/build.py
 # Default Imports
 import pandas as pd
+import matplotlib.pyplot as plt
+from greyatomlib.descriptive_stats.q01_calculate_statistics.build import calculate_statistics
 
 dataframe_1 = pd.read_csv('data/house_prices_multivariate.csv')
 dataframe_2 = pd.read_csv('data/house_prices_copy.csv')
@@ -7,3 +10,10 @@ dataframe_2 = pd.read_csv('data/house_prices_copy.csv')
 
 # Return the correlation value between the SalePrice column for the two loaded datasets
 # Your code here
+
+def correlation():
+    saleprice1=dataframe_1.loc[:,'SalePrice']
+    saleprice2=dataframe_2.loc[:,'SalePrice']
+    return float(saleprice1.corr(saleprice2))
+
+print correlation()
