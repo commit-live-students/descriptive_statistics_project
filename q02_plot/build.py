8de4d88e29521a6ae0fd195c07fb9eac25a3d814
cchopade <cchopade@gmail.com>
2018-02-02 05:02:17 +0000

    Done

--- q02_plot/build.py
+++ q02_plot/build.py
@@ -6,6 +6,17 @@ from greyatomlib.descriptive_stats.q01_calculate_statistics.build import calcula
 dataframe = pd.read_csv('data/house_prices_multivariate.csv')
 sale_price = dataframe.loc[:, 'SalePrice']
 
-
+def plot():
+    mean = sale_price.mean()
+    median = sale_price.median()
+    mode = sale_price.mode()
+    plt.hist(sale_price)
+    plt.plot([mean]*800, range(800), label = 'mean')
+    plt.plot([median]*800,range(800),label = 'median')
+    plt.plot([mode]*800,range(800),label = 'mode')
+    plt.legend()
+    plt.ylim(0,750)
+    plt.xlabel('Sale Price')
+    plt.title('Sale Price distribution with central tendency')
+    plt.show
 # Draw the plot for the mean, median and mode for the dataset
-
