b8fafc3a2a46e7f4bbec3eb191325c12e37d4a60
aditya81295 <patiladitya81295@gmail.com>
2018-02-11 07:33:46 +0000

    Done

--- q04_spearman_correlation/build.py
+++ q04_spearman_correlation/build.py
@@ -3,6 +3,12 @@ import pandas as pd
 
 dataframe_1 = pd.read_csv('data/house_prices_multivariate.csv')
 dataframe_2 = pd.read_csv('data/house_prices_copy.csv')
+df1=dataframe_1.loc[:,"SalePrice"]
+df2=dataframe_2.loc[:,"SalePrice"]
 
 # Your code here
-
+def spearman_correlation():
+    scc=df1.corr(df2,method='spearman')
+    #print scc
+    return scc
+#spearman_correlation()
