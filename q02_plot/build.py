a0521375550d4ed1c5a54ef28bb6b288fb7bb459
shravanighatnekar <joshipoonam0303@gmail.com>
2018-02-14 11:26:35 +0000

    Done

--- q02_plot/build.py
+++ q02_plot/build.py
@@ -8,4 +8,9 @@ sale_price = dataframe.loc[:, 'SalePrice']
 
 
 # Draw the plot for the mean, median and mode for the dataset
-
+def plot():
+    plt.histogram(sale_price)
+    plt.axvline(x=sale_price.mean())
+    plt.axvline(x=sale_price.median())
+    plt.axvline(x=sale_price.mode())
+plt.show()
