deb7bac6c5a873566953239799a6f7a16e88beb1
AshlinKarkada <ashlin@greyatom.com>
2018-03-05 06:54:34 +0000

    Done

--- q04_spearman_correlation/build.py
+++ q04_spearman_correlation/build.py
@@ -1,3 +1,4 @@
+# %load q04_spearman_correlation/build.py
 # Default Import
 import pandas as pd
 
@@ -5,4 +6,18 @@ dataframe_1 = pd.read_csv('data/house_prices_multivariate.csv')
 dataframe_2 = pd.read_csv('data/house_prices_copy.csv')
 
 # Your code here
+
+
+# Default Import
+from greyatomlib import pandas_project as pd
+import pandas as pd
+
+dataframe_1 = pd.read_csv('data/house_prices_multivariate.csv')
+house_price = dataframe_1.loc[:, 'SalePrice']
+
+dataframe_2 = pd.read_csv('data/house_prices_copy.csv')
+weight_of_nasa_space_shuttle = dataframe_2.loc[:, 'SalePrice']
+
+def spearman_correlation():
+    return dataframe_1.SalePrice.corr(dataframe_2.SalePrice, method='spearman')
 
