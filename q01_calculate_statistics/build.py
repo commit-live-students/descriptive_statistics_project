0bd5ac67a43c845376024955def7f3daf6efd6ab
sacvijai <sacvijai@gmail.com>
2018-03-06 07:17:29 +0000

    Done

--- q01_calculate_statistics/build.py
+++ q01_calculate_statistics/build.py
@@ -8,4 +8,6 @@ sale_price = data.loc[:, "SalePrice"]
 
 # Return mean,median & mode for the SalePrice Column
 # Write your code here
-
+def calculate_statistics():
+    mode= sale_price.mode()
+    return np.mean(sale_price),float(np.median(sale_price)),mode[0]
