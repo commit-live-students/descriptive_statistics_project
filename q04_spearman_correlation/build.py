69906b52d7a96419ca2514385688701f1b3b691f
rajeshu80 <rajesh.u80@gmail.com>
2018-03-31 06:46:51 +0000

    Done

--- q04_spearman_correlation/build.py
+++ q04_spearman_correlation/build.py
@@ -6,3 +6,10 @@ dataframe_2 = pd.read_csv('data/house_prices_copy.csv')
 
 # Your code here
 
+SalePrice1 = dataframe_1.loc[:, 'SalePrice']
+SalePrice2 = dataframe_2.loc[:, 'SalePrice']
+# Return the correlation value between the SalePrice column for the two loaded datasets
+# Your code here
+
+def spearman_correlation():
+    return dataframe_1.SalePrice.corr(dataframe_2.SalePrice, method='spearman')
