648d4cb4b6cc086d9e4548f1c3b91662709a1b01
Nikita1993 <nikitajindal93@gmail.com>
2018-05-08 13:09:26 +0000

    Done

--- q02_plot/build.py
+++ q02_plot/build.py
@@ -1,11 +1,24 @@
+# %load q02_plot/build.py
 # Default Imports
 import pandas as pd
 import matplotlib.pyplot as plt
 from greyatomlib.descriptive_stats.q01_calculate_statistics.build import calculate_statistics
+from inspect import getfullargspec
 
 dataframe = pd.read_csv('data/house_prices_multivariate.csv')
 sale_price = dataframe.loc[:, 'SalePrice']
 
-
 # Draw the plot for the mean, median and mode for the dataset
+def plot():
+    mean,median,mode=calculate_statistics()
+    #plt.hist(sale_price)
+    plt.axvline(x=mean,c='r')
+    plt.axvline(x=median,c='y')
+    plt.axvline(x=mode,c='g')
+    plt.show()
+    
+plot()
+
+
+
 
--- q03_pearson_correlation/build.py
+++ q03_pearson_correlation/build.py
@@ -1,3 +1,4 @@
+# %load q03_pearson_correlation/build.py
 # Default Imports
 import pandas as pd
 
@@ -5,5 +6,11 @@ dataframe_1 = pd.read_csv('data/house_prices_multivariate.csv')
 dataframe_2 = pd.read_csv('data/house_prices_copy.csv')
 
 
+def correlation():
+    df1= dataframe_1.loc[:,['SalePrice']]
+    df2= dataframe_2.loc[:,['SalePrice']]
+    return df1.corrwith(df2)['SalePrice']
 # Return the correlation value between the SalePrice column for the two loaded datasets
 # Your code here
+correlation()
+
