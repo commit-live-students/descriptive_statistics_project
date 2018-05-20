5abd29482f885f8d28f35c204b63c5d1fa999a7f
kravigupta <kravi.gupta@gmail.com>
2018-05-20 04:00:08 +0000

    Done

--- q04_spearman_correlation/build.py
+++ q04_spearman_correlation/build.py
@@ -1,3 +1,4 @@
+# %load q04_spearman_correlation/build.py
 # Default Import
 import pandas as pd
 
@@ -5,4 +6,14 @@ dataframe_1 = pd.read_csv('data/house_prices_multivariate.csv')
 dataframe_2 = pd.read_csv('data/house_prices_copy.csv')
 
 # Your code here
+
+def spearman_correlation():
+    # DataFrame.corr(method='pearson', min_periods=1)
+    # method : {âpearsonâ, âkendallâ, âspearmanâ}
+    df = pd.DataFrame(data=dataframe_1.SalePrice)
+    df['S2'] = dataframe_2.SalePrice
+    r = df.corr(method='spearman')
+    return r.iloc[0,1]
+spearman_correlation()
+
 
