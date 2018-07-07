92de66140b8a381b438f50a2d54535a61aa5ab4d
Sumeshks29 <sumeshks29@gmail.com>
2018-07-07 10:42:41 +0000

    Done

--- q02_plot/build.py
+++ q02_plot/build.py
@@ -1,3 +1,4 @@
+# %load q02_plot/build.py
 # Default Imports
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -8,4 +9,18 @@ sale_price = dataframe.loc[:, 'SalePrice']
 
 
 # Draw the plot for the mean, median and mode for the dataset
+
+def plot():
+    plt.figure(figsize=(16,10))
+    plt.hist(sale_price)
+    plt.axvline(np.mean(sale_price),color='Red')
+    plt.axvline(np.median(sale_price),color='Yellow')
+
+    (_, idx, counts) = np.unique(sale_price, return_index=True, return_counts=True)
+    index = idx[np.argmax(counts)]
+    mode = sale_price[index]
+    plt.axvline(mode,color='pink')
+
+plt.show()
+    
 
