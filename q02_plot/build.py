9545d85d094d64f7f98dbafc8f6b8ba722ae9b3a
bhushan45 <bhushan45@users.noreply.github.com>
2018-07-17 14:23:08 +0530

    Update build.py

--- q02_plot/build.py
+++ q02_plot/build.py
@@ -3,6 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from greyatomlib.descriptive_stats.q01_calculate_statistics.build import calculate_statistics
 
+plt.switch_backend('agg')
 dataframe = pd.read_csv('data/house_prices_multivariate.csv')
 sale_price = dataframe.loc[:, 'SalePrice']
 
