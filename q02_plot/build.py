5d99dd5c77fb2fd08dac45876f7372f5a6aadfdf
sahusaurabh65 <sahusaurabh65@gmail.com>
2018-08-06 21:39:33 +0000

    Done

--- q02_plot/build.py
+++ q02_plot/build.py
@@ -1,3 +1,4 @@
+# %load q02_plot/build.py
 # Default Imports
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -8,5 +9,24 @@ dataframe = pd.read_csv('data/house_prices_multivariate.csv')
 sale_price = dataframe.loc[:, 'SalePrice']
 
 
+#def plot():
+   # cc = sale_price.mean(), sale_price.median(), sale_price.mode().iloc[0]
+    #xvline(linewidth=4, color='r')
+def plot():
+    plt.axvline(sale_price.mean())
+    plt.axvline(sale_price.median())
+    plt.axvline(sale_price.mode()[0])
+    plt.hist(sale_price, bins=10)
+    
+plot()
+    
+
+#plot()
+
+
 # Draw the plot for the mean, median and mode for the dataset
+
+
+
+
 
