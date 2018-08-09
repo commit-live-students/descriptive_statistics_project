c73dfad1f07e9ed0188e89df6492dbbdf3edf45a
Sanketakle305 <sanketakle786@gmail.com>
2018-08-09 09:10:26 +0000

    Done

--- q02_plot/build.py
+++ q02_plot/build.py
@@ -1,5 +1,7 @@
+# %load q02_plot/build.py
 # Default Imports
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 from greyatomlib.descriptive_stats.q01_calculate_statistics.build import calculate_statistics
 
@@ -9,4 +11,22 @@ sale_price = dataframe.loc[:, 'SalePrice']
 
 
 # Draw the plot for the mean, median and mode for the dataset
+def plot():
+    mean=np.mean(sale_price)
+    median=sale_price.median()
+    mode=sale_price.mode()
+    mode=mode[0]
+    plt.figure(figsize=(10, 6))
+    plt.hist(sale_price, bins=40)
+    plt.plot([mode]*300, range(300), label='mode')
+    plt.plot([median]*300, range(300), label='median')
+    plt.plot([mean]*300, range(300), label='mean')
+    plt.ylim(0, 250)
+    plt.legend()
+    plt.show()
+    
+
+plot()
+
+
 
