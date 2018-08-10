547c893a00bbcfa25b898c92ed1fe5f482a4a02c
clydemendonca <clyde.mendonca01@gmail.com>
2018-08-10 14:01:04 +0000

    Done

--- q03_pearson_correlation/build.py
+++ q03_pearson_correlation/build.py
@@ -1,3 +1,4 @@
+# %load q03_pearson_correlation/build.py
 # Default Imports
 import pandas as pd
 
@@ -7,3 +8,10 @@ dataframe_2 = pd.read_csv('data/house_prices_copy.csv')
 
 # Return the correlation value between the SalePrice column for the two loaded datasets
 # Your code here
+def correlation():
+    sales_price1 = dataframe_1['SalePrice']
+    sales_price2 = dataframe_2['SalePrice']
+    return sales_price1.corr(sales_price2)
+
+
+
