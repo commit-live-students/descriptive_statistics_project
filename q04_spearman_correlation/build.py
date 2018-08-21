c398bccc6bc6851b96a78029bb5b3ab07edc0359
Sanketakle305 <sanketakle786@gmail.com>
2018-08-21 05:33:03 +0000

    Done

--- q04_spearman_correlation/build.py
+++ q04_spearman_correlation/build.py
@@ -1,3 +1,4 @@
+# %load q04_spearman_correlation/build.py
 # Default Import
 import pandas as pd
 
@@ -5,4 +6,16 @@ dataframe_1 = pd.read_csv('data/house_prices_multivariate.csv')
 dataframe_2 = pd.read_csv('data/house_prices_copy.csv')
 
 # Your code here
+def spearman_correlation():
+    dataframe_1['rank']=dataframe_1['SalePrice'].rank(ascending=1)
+    dataframe_2['rank']=dataframe_2['SalePrice'].rank(ascending=1)
+    d=abs(dataframe_1['rank']-dataframe_2['rank'])
+    df=pd.DataFrame(d**2)
+    sum=df.sum()
+    n=len(df)
+    s_c=1 - 6*sum/(n*(n**2-1))
+    return float(s_c)
+
+spearman_correlation()
+
 
