c669b2ac978774b5e2c4b98d1f4eae00a314b282
Tusharsharma86 <tusharsharma86@gmail.com>
2018-09-01 17:11:49 +0000

    Done

--- q01_calculate_statistics/build.py
+++ q01_calculate_statistics/build.py
@@ -1,11 +1,28 @@
+# %load q01_calculate_statistics/build.py
 # Default Imports
 import numpy as np
 import pandas as pd
+import statistics as s
 
 data = pd.read_csv('data/house_prices_multivariate.csv')
-sale_price = data.loc[:, "SalePrice"]
+sale_price = data.loc[:, 'SalePrice']
 
 
 # Return mean,median & mode for the SalePrice Column
 # Write your code here
+def calculate_statistics():
+    '''Calculate statistics'''
+    mean = np.mean(sale_price)
+    mode = s.mode(sale_price)
+    median = np.median(sale_price)
+    return mean, median, mode
+
+#call the function
+calculate_statistics()
+
+
+
+
+
+
 
--- q02_plot/build.py
+++ q02_plot/build.py
@@ -1,6 +1,9 @@
+# %load q02_plot/build.py
 # Default Imports
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
+import statistics as s
 from greyatomlib.descriptive_stats.q01_calculate_statistics.build import calculate_statistics
 
 plt.switch_backend('agg')
@@ -8,5 +11,21 @@ dataframe = pd.read_csv('data/house_prices_multivariate.csv')
 sale_price = dataframe.loc[:, 'SalePrice']
 
 
+
 # Draw the plot for the mean, median and mode for the dataset
+def plot():
+    ''' Function which plots the histogram and comparing the 3 Ms'''
+    mean = np.mean(sale_price)
+    mode = s.mode(sale_price)
+    median = np.median(sale_price)
+    plt.hist(sale_price)
+    plt.plot([mean]*800,np.arange(0,800),label='mean')
+    plt.plot([median]*800,np.arange(0,800),label='median')
+    plt.plot([mode]*800,np.arange(0,800),label='mode')
+    plt.legend()
+    plt.show()
+
+plot()
+
+
 
