12dca4a8f9d90b413dba8540e8cbefb73d0db4c5
ankit-a-mehta <ankitmehta195@gmail.com>
2018-09-13 08:55:44 +0000

    Done

--- q02_plot/build.py
+++ q02_plot/build.py
@@ -1,3 +1,4 @@
+# %load q02_plot/build.py
 # Default Imports
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -9,4 +10,16 @@ sale_price = dataframe.loc[:, 'SalePrice']
 
 
 # Draw the plot for the mean, median and mode for the dataset
+def plot():
+    mean, median, mode = calculate_statistics()
+    plt.figure(figsize=(10, 6))
+    plt.hist(sale_price, bins=40)
+    plt.plot([mode]*300, range(300), label='mode')
+    plt.plot([median]*300, range(300), label='median')
+    plt.plot([mean]*300, range(300), label='mean')
+    plt.ylim(0, 250)
+    plt.legend()
+    plt.show()
+
+plot()
 
