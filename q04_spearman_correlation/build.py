1daba13131af7d3d538e56597363cc72470599fd
psicktrick <poojarysaurabh932@gmail.com>
2018-09-30 09:05:34 +0000

    Done

--- q04_spearman_correlation/build.py
+++ q04_spearman_correlation/build.py
@@ -1,3 +1,4 @@
+# %load q04_spearman_correlation/build.py
 # Default Import
 import pandas as pd
 
@@ -5,4 +6,9 @@ dataframe_1 = pd.read_csv('data/house_prices_multivariate.csv')
 dataframe_2 = pd.read_csv('data/house_prices_copy.csv')
 
 # Your code here
+def spearman_correlation():
+    
+    return dataframe_1['SalePrice'].corr(dataframe_2['SalePrice'], method='spearman')
+
+
 
