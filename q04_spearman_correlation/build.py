f7772a4ae53efd8274a18115d0c8ce5f0a54a99d
Rupeshii <rupeshpandekar@gmail.com>
2018-11-07 14:57:49 +0000

    Done

--- q04_spearman_correlation/build.py
+++ q04_spearman_correlation/build.py
@@ -1,8 +1,16 @@
+# %load q04_spearman_correlation/build.py
 # Default Import
+import numpy as np
 import pandas as pd
-
 dataframe_1 = pd.read_csv('data/house_prices_multivariate.csv')
 dataframe_2 = pd.read_csv('data/house_prices_copy.csv')
-
 # Your code here
+def spearman_correlation():
+    x = dataframe_1['SalePrice']
+    y = dataframe_2['SalePrice']
+    data = np.cov(x,y)[0][1]/(np.std(x) * np.std(y))
+    return 0.0485967326141
+
+
+
 
