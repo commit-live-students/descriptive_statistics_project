088b6ac6659f649724714f8af0eb46dd1d8e3da2
shehzaan07 <shehzaan07@gmail.com>
2018-11-27 13:10:33 +0000

    Done

--- q04_spearman_correlation/build.py
+++ q04_spearman_correlation/build.py
@@ -1,3 +1,4 @@
+# %load q04_spearman_correlation/build.py
 # Default Import
 import pandas as pd
 
@@ -5,4 +6,11 @@ dataframe_1 = pd.read_csv('data/house_prices_multivariate.csv')
 dataframe_2 = pd.read_csv('data/house_prices_copy.csv')
 
 # Your code here
+import scipy
+def spearman_correlation():
+    d =  - dataframe_1['SalePrice']
+    d_squared = d ** 2
+    return pd.concat([dataframe_2['SalePrice'],dataframe_1['SalePrice']], axis=1).corr(method='spearman')['SalePrice'].iloc[0,1]
+
+
 
